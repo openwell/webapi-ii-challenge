f4a3696250a8ef7b83068f39be8274aa8be59ad6
openwell <timileyinojo@yahoo.com>
2019-07-09 14:21:42 +0100

    added a route for making user posts

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("Hello World!");
 });
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  try {
+    db.insert(req.body).then(data => {
+      res.status(201).json({
+        message: data
+      });
+    });
+  } catch (err) {
+    res.json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
